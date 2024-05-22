cb5f8a4be4cca0271a59bfb0ff586c9abf2667e6
Eörs Szathmáry <eorsjr@gmail.com>
2024-05-22 19:10:13 +0200

    feat: :sparkles: add table support with click-to-copy ability

---
+++ scripts/click-to-copy.js
@@ -0,0 +1,36 @@
+/* This code is responsible for copying table content on click. */
+
+function copyTableCell(text) {
+    navigator.clipboard.writeText(text);
+}
+
+document.querySelectorAll('table td').forEach(cell => {
+    cell.addEventListener('click', function () {
+        const text = this.textContent || this.innerText;
+        copyTableCell(text);
+    });
+});
+
+function copyTable(copyButton) {
+    let table = copyButton.previousElementSibling;
+    let columnWidths = [];
+
+    for (let row of table.rows) {
+        for (let i = 0; i < row.cells.length; i++) {
+            let cell = row.cells[i];
+            columnWidths[i] = Math.max(columnWidths[i] || 0, cell.innerText.length);
+        }
+    }
+
+    let tableText = '';
+
+    for (let row of table.rows) {
+        for (let i = 0; i < row.cells.length; i++) {
+            let cell = row.cells[i];
+            tableText += cell.innerText.padEnd(columnWidths[i] + 1, ' ');
+        }
+        tableText += '\n';
+    }
+
+    navigator.clipboard.writeText(tableText);
+}
