d44ca34aa39d2bf686fdb17a78aed5eb6074e4bf
Eörs Szathmáry <eorsjr@gmail.com>
2024-05-24 17:31:46 +0200

    feat: :sparkles: make copied text Excel-friendly

--- scripts/click-to-copy.js
+++ scripts/click-to-copy.js
@@ -1,9 +1,19 @@
-/* This code is responsible for copying table content on click. */
+/**
+ * This code is responsible for copying table content on click.
+ */
 
+/**
+ * Copies the provided text to the clipboard.
+ * @param {*} text - The text to be copied to the clipboard.
+ */
 function copyTableCell(text) {
     navigator.clipboard.writeText(text);
 }
 
+/**
+ * Adds click event listeners to all table cells (td) to copy their content to the clipboard.
+ * When a cell is clicked, its text content is copied.
+ */
 document.querySelectorAll('table td').forEach(cell => {
     cell.addEventListener('click', function () {
         const text = this.textContent || this.innerText;
@@ -11,25 +21,22 @@ document.querySelectorAll('table td').forEach(cell => {
     });
 });
 
+/**
+ * Copies the entire content of a table to the clipboard.
+ * The table is found by traversing from the provided button to the previous sibling element containing the table.
+ * @param {*} copyButton - The button element that triggers the table copy action.
+ */
 function copyTable(copyButton) {
     let table = copyButton.previousElementSibling.querySelector("table");
-    let columnWidths = [];
-
-    for (let row of table.rows) {
-        for (let i = 0; i < row.cells.length; i++) {
-            let cell = row.cells[i];
-            columnWidths[i] = Math.max(columnWidths[i] || 0, cell.innerText.length);
-        }
-    }
-
     let tableText = '';
 
     for (let row of table.rows) {
+        let rowText = '';
         for (let i = 0; i < row.cells.length; i++) {
             let cell = row.cells[i];
-            tableText += cell.innerText.padEnd(columnWidths[i] + 1, ' ');
+            rowText += cell.innerText + '\t';
         }
-        tableText += '\n';
+        tableText += rowText.trimEnd() + '\n';
     }
 
     navigator.clipboard.writeText(tableText);
