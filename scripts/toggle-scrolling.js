70acc3f615d205adae1a33e4861a6d36716baea1
Eörs Szathmáry <eorsjr@gmail.com>
2024-05-28 14:27:33 +0200

    fix: :bug: update scrolling prevention

--- scripts/toggle-scrolling.js
+++ scripts/toggle-scrolling.js
@@ -7,14 +7,27 @@
  * If either the mobile menu or lightbox is visible, scrolling is disabled.
  * If neither overlay is visible, scrolling is enabled.
  */
+
+let scrollPos = 0;
+
 function toggleScrolling() {
+
     if (mobileMenuVisible || lightboxVisible) {
         // If mobile menu or lightbox is visible, disable scrolling
+        scrollPos = window.scrollY;
+        document.body.style.top = "-" + scrollPos + "px"; // Set top to current scroll position
+        document.body.style.position = "fixed"; // Fix body position
         document.documentElement.style.overflow = "hidden"; // Disable vertical scrolling
         document.body.scroll = "no"; // Disable horizontal scrolling
     } else {
         // If neither overlay is visible, enable scrolling
-        document.documentElement.style.overflow = "scroll"; // Enable vertical scrolling
-        document.body.scroll = "yes"; // Enable horizontal scrolling
+        if (document.body.style.position == "fixed") {
+            document.body.style.position = "static"; // Set body position to static
+            window.scrollTo({ top: scrollPos, behavior: "instant" }); // jump to saved position
+            document.body.style.top = "auto"; //reset top
+            scrollPos = 0; // set scroll position to 0
+            document.documentElement.style.overflow = "scroll"; // Enable vertical scrolling
+            document.body.scroll = "yes"; // Enable horizontal scrolling
+        }
     }
 }
