197b41adde0e57d7ee862cf6aeb99cf5f248511c
Eörs Szathmáry <eorsjr@gmail.com>
2024-06-06 11:45:40 +0200

    refactor: :art: rename navigation bar to navigation component
    Rename navigation bar to navigation component to reflect that it can become a navigation bar or a navigation rail depending on the context.

--- scripts/hide-nav-component.js
+++ scripts/hide-nav-component.js
@@ -1,11 +1,11 @@
 let prevScrollPos = window.scrollY;
 
 /**
- * Hides or reveals the navigation bar based on the scroll direction.
- * The navigation bar is shown when scrolling up or at the top of the page,
+ * Hides or reveals the navigation component based on the scroll direction.
+ * The navigation component is shown when scrolling up or at the top of the page,
  * and hidden when scrolling down. Adjustments are made based on screen orientation.
  */
-function hideNavigationBar() {
+function hideNavComponent() {
     let currentScrollPos = window.scrollY;
 
     // Check portrait orientation
@@ -13,11 +13,11 @@ function hideNavigationBar() {
         // Check if navigation drawer and lightbox are not visible
         if ($(".navigation-drawer").css("display") === "none" && $(".lightbox").css("display") === "none") {
             if (prevScrollPos > currentScrollPos || currentScrollPos < 1) {
-                // Show the navigation bar when scrolling up or at the top of the page
-                document.querySelector(".navigation-bar").style.bottom = "0";
+                // Show the navigation component when scrolling up or at the top of the page
+                document.querySelector(".navigation-component").style.bottom = "0";
             } else {
-                // Hide the navigation bar when scrolling down
-                document.querySelector(".navigation-bar").style.bottom = "-150px"; // Adjust as necessary
+                // Hide the navigation component when scrolling down
+                document.querySelector(".navigation-component").style.bottom = "-150px"; // Adjust as necessary
             }
         }
     }
@@ -26,15 +26,15 @@ function hideNavigationBar() {
 }
 
 /**
- * Ensures the navigation bar is visible in landscape mode.
+ * Ensures the navigation component is visible in landscape mode.
  */
 function handleOrientationOrResize() {
     if (window.innerHeight < window.innerWidth) {
-        document.querySelector(".navigation-bar").style.bottom = "0";
+        document.querySelector(".navigation-component").style.bottom = "0";
     }
 }
 
 // Add event listeners to trigger the hideNavigation function
-window.addEventListener("scroll", hideNavigationBar);
+window.addEventListener("scroll", hideNavComponent);
 window.addEventListener("resize", handleOrientationOrResize);
 window.addEventListener("orientationchange", handleOrientationOrResize);
